89e8d083d9ecc01d255e53c044cfb2628e5b890b
Alex Jose <alexjose4u@gmail.com>
2014-08-10 20:54:26 +0530

    Fixes #2, Values of meta box fields are saved.
    Values are saved in the meta table with multiple values for keys.

--- inc/cptr-metaboxes.php
+++ inc/cptr-metaboxes.php
@@ -1,6 +1,8 @@
 <?php
 add_action('add_meta_boxes', 'cptr_add_metaboxes');
 
+add_action('save_post', 'cprt_save_relations_metabox_data');
+
 function cptr_add_metaboxes() {
     global $cpt_relations;
     global $post;
@@ -26,6 +28,8 @@ function cptr_add_metaboxes() {
 function cpt_relations_metabox($post) {
     global $wpdb;
     global $cpt_relations;
+
+    wp_nonce_field('cprt_metabox', 'cprt_metabox_nonce');
 
     $selectedPosts = array();
     $allPostsIDs = array();
@@ -73,4 +77,79 @@ function cpt_relations_metabox($post) {
         ?>
     </table>
     <?php
+}
+
+function cprt_save_relations_metabox_data($post_id) {
+    global $cpt_relations;
+
+    /*
+     * We need to verify this came from our screen and with proper authorization,
+     * because the save_post action can be triggered at other times.
+     */
+    // Check if our nonce is set.
+    if (!isset($_POST['cprt_metabox_nonce'])) {
+        return;
+    }
+
+    // Verify that the nonce is valid.
+    if (!wp_verify_nonce($_POST['cprt_metabox_nonce'], 'cprt_metabox')) {
+
+        return;
+    }
+
+    // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return;
+    }
+
+    // Check the user's permissions.
+    if (!isset($_POST['post_type'])) {
+        return;
+    }
+
+    if (!current_user_can('edit_post', $post_id)) {
+        return;
+    }
+
+    foreach ($cpt_relations as $key => $relation) {
+        if ($relation['from_pt'] != get_post_type($post_id)) {
+            unset($cpt_relations[$key]);
+        }
+    }
+    foreach ($cpt_relations as $key => $relation) {
+        $items = $_POST[$relation['key']];
+        $items = explode(",", $items);
+
+        foreach ($items as $key => $item) {
+            if (!empty($item) && !is_numeric($item)) {
+                $post = array(
+                    'post_title' => (ucwords($item)),
+                    'post_type' => $relation['to_pt'],
+                    'post_status' => 'publish',
+                );
+                $items[$key] = (string) wp_insert_post($post, $error);
+            }
+        }
+
+//      Find the new artists added and deleted old artist by comparing the
+//      new artist list with the old artists list stored in the table if any.
+        $old_items = get_post_meta($post_id, $relation['key']);
+        $items_to_add = array_diff($items, $old_items);
+        $items_to_delete = array_diff($old_items, $items);
+
+        if (count($old_items) == count($items_to_delete)) {
+//          All Artists Are New. Delete All At Once & Add One By One
+            delete_post_meta($post_id, $relation['key']);
+        } else {
+//          Delete the artists present in db but not in submitted form
+            foreach ($items_to_delete as $item) {
+                delete_post_meta($post_id, $relation['key'], $item);
+            }
+        }
+
+//      Add the new artist submitted in form one by one
+        foreach ($items_to_add as $item) {
+            add_post_meta($post_id, $relation['key'], $item);
+        }
+    }
 }
